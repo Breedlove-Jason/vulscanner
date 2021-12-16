361754ee18a96fb95d4ac75f9136c5c43c5bacad
anonymous164 <j@jasonbreedlove.com>
2021-12-15 16:04:53 -0800

    bug in vulbanners.txt

--- vulscan.py
+++ vulscan.py
@@ -8,3 +8,11 @@ print('\n')
 target = PortScan(targets_ip, port_number)
 target.scan()
 
+with open(vul_file, 'r') as file:
+    count = 0
+    for banner in target.banners:
+        file.seek(0)
+        for line in file.readlines():
+            if line.strip() in banner:
+                print('[!!] VULNERABLE BANNER: ' + banner + 'ON PORT: ' + str(target.open_ports[count]))
+        count += 1
